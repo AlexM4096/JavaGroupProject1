3fce752988772bb56ba647caef1dbf903adb785d
Alexander Markov <alex.m.game.jam@gmail.com>
2023-11-13 12:25:24 +1000

    Add jsoup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javagroupproject1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.jsoup;
 
 
     opens com.example.javagroupproject1 to javafx.fxml;
