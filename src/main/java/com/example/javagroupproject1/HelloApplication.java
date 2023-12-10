d63337f98b5908d1ceeec191a3f900edf89125a6
Shevchuk Polina <shevchukpolina1979@gmail.com>
2023-12-11 02:25:49 +1000

    странички

--- src/main/java/com/example/javagroupproject1/HelloApplication.java
+++ src/main/java/com/example/javagroupproject1/HelloApplication.java
@@ -1,5 +1,4 @@
 package com.example.javagroupproject1;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,9 +9,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("recipe.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+        /*stage.setTitle("Hello!");
         PageController.getInstance().setStage(stage);
-        PageController.getInstance().change(new CatalogPage());
+        PageController.getInstance().change(new CatalogPage());*/
     }
 
     public static void main(String[] args) {
