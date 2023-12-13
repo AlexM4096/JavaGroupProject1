cb7fc7bb2d209f8f7ee9b5e4d7f049f02aaafb36
Shevchuk Polina <shevchukpolina1979@gmail.com>
2023-12-13 21:35:17 +1000

    страницы доделала

--- src/main/java/com/example/javagroupproject1/HelloApplication.java
+++ src/main/java/com/example/javagroupproject1/HelloApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("filt.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("addrecipe.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
